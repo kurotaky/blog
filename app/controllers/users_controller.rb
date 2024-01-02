0ae0fc27356e0d853fbf43ecdb66fa23750c5c90
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2024-01-02 15:36:35 +0900

    UsersController

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    # only proceed with pretty names
+    if @user and @user.username and @user.username.size > 0
+
+      # create randome nonce
+      @user.eth_nonce = SecureRandom.uuid
+
+      if @user.eth_address
+        # make sure the eth address is valid
+        address = Eth::Address.new @user.eth_address
+        if address.valid?
+          if @user.save
+            redirect_to login_path, notice: "Successfully created an account, you may now log in."
+          else
+            redirect_to login_path, alert: "Account already exists! Try to log in instead!"
+          end
+        else
+          flash.now[:alert] = "Invalid Ethereum address!"
+          render :new
+        end
+      else
+        flash.now[:alert] = "Failed to get Ethereum address!"
+        render :new
+      end
+    else
+      flash.now[:alert] = "Please choose a name (length > 0)!"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :eth_address)
+  end
+end
