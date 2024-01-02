e33b311198307f6dd9d52ea4b8d14b00dbbf4802
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2024-01-02 15:27:17 +0900

    validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :eth_address, presence: true, uniqueness: true
+  validates :eth_nonce, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 end
