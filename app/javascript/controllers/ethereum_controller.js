204e00d730a7834b008726d1da2bbc42eebd4dd3
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2024-01-07 03:34:13 +0900

    Log-in with Ethereum

--- app/javascript/controllers/ethereum_controller.js
+++ app/javascript/controllers/ethereum_controller.js
@@ -1,13 +1,22 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["ethAddress", "newUser"];
+  static targets = ["ethAddress", "newUser", "ethSignature", "ethMessage", "newSession"];
 
   connectToWallet() {
     if (typeof window.ethereum !== 'undefined') {
       this.requestAccounts();
     } else {
       console.error("Ethereum object not found");
+    }
+  }
+
+  connect() {
+    if (typeof window.ethereum === 'undefined') {
+      this.element.textContent = "No Ethereum Context Available";
+      this.element.disabled = true;
+    } else {
+      this.hideFields();
     }
   }
 
@@ -24,4 +33,49 @@ export default class extends Controller {
       console.error("Error requesting accounts:", error);
     }
   }
+
+  async loginWithEthereum() {
+    this.element.disabled = true;
+
+    const accounts = await this.requestAccounts();
+    const etherbase = accounts[0];
+    const nonce = await this.getUuidByAccount(etherbase);
+
+    if (nonce) {
+      const customTitle = "blog";
+      const requestTime = new Date().getTime();
+      const message = `${customTitle},${requestTime},${nonce}`;
+      const signature = await this.personalSign(etherbase, message);
+
+      this.ethMessageTarget.value = message;
+      this.ethAddressTarget.value = etherbase;
+      this.ethSignatureTarget.value = signature;
+      this.newSessionTarget.submit();
+    } else {
+      this.ethMessageTarget.value = "Please sign up first!";
+    }
+  }
+
+  hideFields() {
+    this.ethMessageTarget.hidden = true;
+    this.ethAddressTarget.hidden = true;
+    this.ethSignatureTarget.hidden = true;
+  }
+
+  async requestAccounts() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    return accounts;
+  }
+
+  async personalSign(account, message) {
+    const signature = await ethereum.request({ method: 'personal_sign', params: [message, account] });
+    return signature;
+  }
+
+  async getUuidByAccount(account) {
+    const response = await fetch(`/api/v1/users/${account}`);
+    const nonceJson = await response.json();
+    return nonceJson ? nonceJson[0].eth_nonce : null;
+  }
+
 }
