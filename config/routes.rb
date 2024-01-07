6837840d66c68564fed3b4bf911e7b11d6e0822f
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2024-01-07 12:42:09 +0900

    Login only

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,6 @@ Rails.application.routes.draw do
   post "logout", to: "sessions#destroy"
   get "logout", to: "sessions#destroy"
 
-  resources :users, only: [:new, :create]
-
   namespace :api do
     namespace :v1 do
       resources :users
